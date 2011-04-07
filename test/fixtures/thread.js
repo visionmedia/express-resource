efcd806214f02dca2d46cc59cff807ecef51033c
Tj Holowaychuk <tj@vision-media.ca>
2011-04-07 00:22:21 -0700

    added lingo submod

--- test/fixtures/thread.js
+++ test/fixtures/thread.js
@@ -12,17 +12,17 @@ exports.create = function(req, res){
 };
 
 exports.show = function(req, res){
-  res.send('show thread ' + req.params.thread_id);
+  res.send('show thread ' + req.params.thread_id + ' of forum ' + req.params.forum_id);
 };
 
 exports.edit = function(req, res){
-  res.send('edit thread ' + req.params.thread_id);
+  res.send('edit thread ' + req.params.thread_id + ' of forum ' + req.params.forum_id);
 };
 
 exports.update = function(req, res){
-  res.send('update thread ' + req.params.thread_id);
+  res.send('update thread ' + req.params.thread_id + ' of forum ' + req.params.forum_id);
 };
 
 exports.destroy = function(req, res){
-  res.send('destroy thread ' + req.params.thread_id);
+  res.send('destroy thread ' + req.params.thread_id + ' of forum ' + req.params.forum_id);
 };
