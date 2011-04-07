a68960bc2587e86a905ca719d3f6585b89efc146
Tj Holowaychuk <tj@vision-media.ca>
2011-04-07 09:19:35 -0700

    added another test file

---
+++ test/resource.content-negotiation.test.js
@@ -0,0 +1,14 @@
+
+/**
+ * Module dependencies.
+ */
+var assert = require('assert')
+  , express = require('express')
+  , should = require('should')
+  , Resource = require('../');
+
+module.exports = {
+  'test content-negotiation via extension': function(){
+    
+  }
+};
